735f17c9b5d24c5b2bc466e130d08f4cb93d1cc5
shariqshahzad789 <shariqshahzad789@gmail.com>
2022-11-21 12:29:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,114 @@
+// var subject1 = prompt("english");
+// var subject2 = prompt("Math");
+// var subject3 = prompt("Physics");
+// var subject4 = prompt("Chemistry");
+// var subject5 = prompt("Biology");
+
+// console.log(subject1)
+// console.log(subject2)
+// console.log(subject3)
+// console.log(subject4)
+// console.log(subject5)
+
+// var officer = "no";
+// var assistant = "no";
+
+
+// if(officer == "yes"){
+//     console.log("document Submitted to officer");
+// } else if(assistant == "yes") {
+//     console.log("document Submitted to assistant");
+// } else{
+//     console.log("wapsi")
+// }
+
+
+// == EQUALITY
+// != NOT EQUALITY
+// === EQUALITY AND DATA TYPE
+// !== NOT EQUALITY AND DATA TYPE
+// > GREATER THAN 
+// < LESS THAN
+// <= GREATER THAN EQUAL TO
+// <= LESS THAN EQUAL TO
+
+
+
+// var num = prompt("Enter Number");
+// if (num !=10) {
+//     if("Num > 5"){
+//         console.log("Number is Greater than 5")
+//     }else {
+//         console.log("Number is not Equal to 10")
+//     }
+// }
+
+
+
+
+var subject1 = +prompt("english");
+var subject2 = +prompt("Math");
+var subject3 = +prompt("Physics");
+var subject4 = +prompt("Chemistry");
+var subject5 = +prompt("Biology");
+
+var totalMarks = 500;
+
+console.log(subject1)
+console.log(subject2)
+console.log(subject3)
+console.log(subject4)
+console.log(subject5)
+
+var total = subject1 + subject2 + subject3 + subject4 + subject5;
+
+console.log(total);
+
+var perc = total / totalMarks * 100;
+
+if (perc>80) {
+    alert("Your Grade is A one")
+}else if("perc>70 && <80"){
+    alert("Your Grade is A")
+}else if("perc>60"){
+    alert("Your Grade is B")
+}else if("perc>50"){
+    alert("Your Grade is C")
+}else if("perc>40"){
+    alert("Your Grade is D")
+}else if("perc>30"){
+    alert("Your Grade is E")
+}
+
+// var fruit1 = "orange"
+// var fruit2 = "apple"
+// var fruit3 = "mango"
+// var fruit4 = "grape"
+// var fruit5 = "berries"
+
+
+// var fruits = ["orange","apple","mango","grape","berries"];
+
+// console.log(Fruits[0]);
+
+
+
+// var cities =["Islamabad","Karachi","Lahore"];
+// alert ( "Welcometo"+cities [4])
+
+// cities.push ("Faisalabad","Gujrawala")
+// console.log("cities=>",cities)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
